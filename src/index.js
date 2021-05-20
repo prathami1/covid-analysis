66792affa21647b9b6ac1204791c9d16fd5c05d7
Pratham <62070812+prathami1@users.noreply.github.com>
2021-05-19 21:12:11 -0400

    Updated Security Measures 🔑

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVital';
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
@@ -13,7 +13,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
