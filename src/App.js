f48f20581bc54596c3a82e0193b36dad716f91d5
Pratham <62070812+prathami1@users.noreply.github.com>
2021-05-19 21:12:37 -0400

    Updated Security Measures 🔑

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom'
 
 //pages
-import Datasets from './pages/Datasets'
-import Time from './pages/Time'
-import News from './pages/News'
-import Vac from './pages/Vac'
+import Datasets from './Datasets'
+import Time from './Time'
+import News from './News'
+import Vac from './Vac'
 
 function App() {
     return(
